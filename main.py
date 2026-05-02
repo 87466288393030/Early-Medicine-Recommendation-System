2e8468cf74badd584e8b110817a05566b6c43acf
Suphal Thapa04 <suphalthapa008@gmail.com>
2026-05-02 11:52:39 +0545

    initial commit

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ diets = pd.read_csv("datasets/diets.csv")
 
 
 # load model===========================================
-svc = pickle.load(open('models/svc.pkl','rb'))
+svc = pickle.load(open('model/svc.pkl','rb'))
 
 
 #============================================================
